bc492915710584a7e045a084f686cc681ec18b55
Ben Hirschberg <bhirschb@cyberarmor.io>
2020-12-27 10:52:19 +0200

    env sh

---
+++ env.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+export CLAIR_URL=http://35.246.251.137:6060
+export OCIMAGE_URL=http://localhost:8080
+export AWS_ACCESS_KEY_ID=a
+export AWS_SECRET_ACCESS_KEY=a
+export AWS_REGION=a
+export S3_BUCKET=a
