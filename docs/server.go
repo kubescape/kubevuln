2779fed1ad04f0ea30826c129c8ae094c63d72bf
matanshk <90377897+matanshk@users.noreply.github.com>
2023-01-10 15:31:55 +0000

    Update server.go

--- docs/server.go
+++ docs/server.go
@@ -53,6 +53,6 @@ func NewOpenAPIUIHandler() http.Handler {
 	openAPIUIHandler = middleware.RapiDoc(RapiDocOpts, openAPIUIHandler)
 	openAPIUIHandler = middleware.SwaggerUI(opts, openAPIUIHandler)
 
-	return openAPIUIHandler
 	
+	return openAPIUIHandler
 }
