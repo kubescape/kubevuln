083ba9466f0892699552e1a49c20554d9cece033
Matthias Bertschy <matthias.bertschy@gmail.com>
2023-03-03 15:01:03 +0100

    add config file handling with viper
    Signed-off-by: Matthias Bertschy <matthias.bertschy@gmail.com>

--- cmd/http/main.go
+++ cmd/http/main.go
@@ -14,6 +14,7 @@ import (
 	"github.com/kubescape/go-logger"
 	"github.com/kubescape/go-logger/helpers"
 	"github.com/kubescape/kubevuln/adapters/v1"
+	"github.com/kubescape/kubevuln/config"
 	"github.com/kubescape/kubevuln/controllers"
 	"github.com/kubescape/kubevuln/core/services"
 	"github.com/kubescape/kubevuln/repositories"
@@ -24,11 +25,17 @@ func main() {
 	// Create context that listens for the interrupt signal from the OS.
 	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
 	defer stop()
+
+	config, err := config.LoadConfig(".")
+	if err != nil {
+		logger.L().Ctx(ctx).Fatal("load config error", helpers.Error(err))
+	}
+
 	// to enable otel, set OTEL_COLLECTOR_SVC=otel-collector:4317
 	if otelHost, present := os.LookupEnv("OTEL_COLLECTOR_SVC"); present {
 		ctx = logger.InitOtel("kubevuln",
 			os.Getenv("RELEASE"),
-			os.Getenv("ACCOUNT_ID"),
+			os.Getenv(config.AccountID),
 			url.URL{Host: otelHost})
 		defer logger.ShutdownOtel(ctx)
 	}
@@ -36,9 +43,9 @@ func main() {
 	repository := repositories.NewMemoryStorage() // TODO add real storage
 	sbomAdapter := v1.NewSyftAdapter()
 	cveAdapter, _ := v1.NewGrypeAdapter(ctx)
-	platform := v1.NewArmoAdapter("", "") // TODO set with config file
+	platform := v1.NewArmoAdapter(config.AccountID, config.EventReceiverURL)
 	service := services.NewScanService(sbomAdapter, repository, cveAdapter, repository, platform)
-	controller := controllers.NewHTTPController(service, 1) // TODO set with config file
+	controller := controllers.NewHTTPController(service, config.ScanConcurrency)
 
 	gin.SetMode(gin.ReleaseMode)
 	router := gin.Default()
