fda696180da5265fcd11a27fa14ccaa0bab66cb8
Matthias Bertschy <matthias.bertschy@gmail.com>
2023-03-03 15:01:03 +0100

    add config file handling with viper
    Signed-off-by: Matthias Bertschy <matthias.bertschy@gmail.com>

---
+++ config/config.go
@@ -0,0 +1,26 @@
+package config
+
+import "github.com/spf13/viper"
+
+type Config struct {
+	AccountID        string `mapstructure:"ACCOUNT_ID"`
+	EventReceiverURL string `mapstructure:"EVENT_RECEIVER_URL"`
+	ScanConcurrency  int    `mapstructure:"SCAN_CONCURRENCY"`
+}
+
+// LoadConfig reads configuration from file or environment variables.
+func LoadConfig(path string) (config Config, err error) {
+	viper.AddConfigPath(path)
+	viper.SetConfigName("app")
+	viper.SetConfigType("env")
+
+	viper.AutomaticEnv()
+
+	err = viper.ReadInConfig()
+	if err != nil {
+		return
+	}
+
+	err = viper.Unmarshal(&config)
+	return
+}
