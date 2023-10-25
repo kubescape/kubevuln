6494944a638230b5a2c157a1ebda0ac7714dfa36
David Wertenteil <dwertent@armosec.io>
2023-10-25 12:11:26 +0300

    get ns from config
    Signed-off-by: David Wertenteil <dwertent@armosec.io>

--- cmd/http/main.go
+++ cmd/http/main.go
@@ -24,10 +24,6 @@ import (
 	"go.opentelemetry.io/contrib/instrumentation/github.com/gin-gonic/gin/otelgin"
 )
 
-const (
-	defaultNamespace = "kubescape"
-)
-
 func main() {
 	ctx := context.Background()
 	c, err := config.LoadConfig("/etc/config")
@@ -61,7 +57,7 @@ func main() {
 
 	var storage *repositories.APIServerStore
 	if c.Storage {
-		storage, err = repositories.NewAPIServerStorage(getDefaultStorageNamespace())
+		storage, err = repositories.NewAPIServerStorage(clusterData.Namespace)
 		if err != nil {
 			logger.L().Ctx(ctx).Fatal("storage initialization error", helpers.Error(err))
 		}
@@ -130,11 +126,4 @@ func main() {
 	controller.Shutdown()
 
 	logger.L().Info("kubevuln exiting")
-}
-
-func getDefaultStorageNamespace() string {
-	if ns, ok := os.LookupEnv("NAMESPACE"); ok && ns != "" {
-		return ns
-	}
-	return defaultNamespace
 }
