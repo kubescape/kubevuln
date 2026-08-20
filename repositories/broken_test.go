7141b22b4b39e9ff8e12ebe8abb068b8d4857f69
H'sain Mohamed amine <aminehs623@gmail.com>
2026-08-20 20:39:28 +0200

    test: add coverage for remaining BrokenStore methods
    Signed-off-by: H'sain Mohamed amine <aminehs623@gmail.com>

--- repositories/broken_test.go
+++ repositories/broken_test.go
@@ -31,3 +31,40 @@ func TestBrokenStore_StoreSBOM(t *testing.T) {
 	err := b.StoreSBOM(context.TODO(), domain.SBOM{}, false)
 	assert.Error(t, err)
 }
+
+func TestBrokenStore_GetContainerProfile(t *testing.T) {
+	b := NewBrokenStorage()
+	_, err := b.GetContainerProfile(context.TODO(), "", "")
+	assert.Error(t, err)
+}
+
+func TestBrokenStore_GetCVESummary(t *testing.T) {
+	b := NewBrokenStorage()
+	summary, err := b.GetCVESummary(context.TODO())
+	assert.NoError(t, err)
+	assert.NotNil(t, summary)
+}
+
+func TestBrokenStore_StoreCVESummary(t *testing.T) {
+	b := NewBrokenStorage()
+	err := b.StoreCVESummary(context.TODO(), domain.CVEManifest{}, domain.CVEManifest{}, false)
+	assert.Error(t, err)
+}
+
+func TestBrokenStore_StoreCVESummaryStub(t *testing.T) {
+	b := NewBrokenStorage()
+	err := b.StoreCVESummaryStub(context.TODO(), "")
+	assert.Error(t, err)
+}
+
+func TestBrokenStore_StoreVEX(t *testing.T) {
+	b := NewBrokenStorage()
+	err := b.StoreVEX(context.TODO(), domain.CVEManifest{}, domain.CVEManifest{}, false)
+	assert.Error(t, err)
+}
+
+func TestBrokenStore_DeleteSBOM(t *testing.T) {
+	b := NewBrokenStorage()
+	err := b.DeleteSBOM(context.TODO(), "")
+	assert.Error(t, err)
+}
