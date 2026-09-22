250a988cdcc51ecaf801e657c129eaa11449883c
bhuvan-somisetty <somisettybhuvan5@gmail.com>
2026-09-23 05:23:47 +0530

    test(registryauth): add test coverage for nil credential fetch return
    Signed-off-by: bhuvan-somisetty <somisettybhuvan5@gmail.com>

--- internal/registryauth/cache.go
+++ internal/registryauth/cache.go
@@ -159,9 +159,6 @@ func (c *credentialCache) get(ctx context.Context, key string, fetch credentialF
 		if res.Err != nil {
 			return nil, res.Err
 		}
-		if res.Val == nil {
-			return nil, nil
-		}
 		return res.Val.(*image.RegistryCredentials), nil
 	}
 }
